c5b3f2fec5d716ad828a82b22bd2daa31061bb11
Dulanga-21 <dulangamanusha1@gmail.com>
2024-04-21 00:52:08 +0530

    modified pages

--- contactUs.php
+++ contactUs.php
@@ -13,7 +13,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
 
-    <title>SnapX Photographer Page</title>
+    <title>Contact Us</title>
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
